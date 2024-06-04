af11efb620575bbc50024cad20a6cfc9290f6a82
tomikases5 <chlapik.tomas@gmail.com>
2024-06-04 19:49:45 +0200

    Interupt

--- src/main.c
+++ src/main.c
@@ -5,35 +5,48 @@
 #include "milis.h"
 //#include "delay.h"
 //#include "uart1.h"
-
+bool on = true;
+INTERRUPT_HANDLER(EXTI_PORTD_IRQHandler, 6)
+{
+    on = !on;
+    if(on) GPIO_WriteHigh(GPIOD, GPIO_PIN_3); else GPIO_WriteLow(GPIOD, GPIO_PIN_3);
+}
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
+    //GPIO_Init(GPIO_MODE_OUT_OD_LOW_SLOW);
+    GPIO_Init(GPIOE,GPIO_PIN_0,GPIO_MODE_OUT_OD_LOW_SLOW); //D2
+    GPIO_Init(GPIOD,GPIO_PIN_1,GPIO_MODE_IN_PU_IT);        //D7
+    GPIO_Init(GPIOD,GPIO_PIN_3,GPIO_MODE_OUT_PP_LOW_SLOW); //D8
+
+    EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOD, EXTI_SENSITIVITY_FALL_ONLY);     // nastavení priority přerušení     
+    ITC_SetSoftwarePriority(ITC_IRQ_PORTD, ITC_PRIORITYLEVEL_1);     // povolení přeruření     
+    enableInterrupts();
+
 
     init_milis();
     //init_uart1();
 }
 
 
-int main(void)
-{
+int main(void){ 
+
   
     uint32_t time = 0;
 
     init();
-
+    
     while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
+
+
+
+        if (milis() - time > 1000 ) {
+            GPIO_WriteHigh(GPIOE, GPIO_PIN_0);
+            
+            GPIO_WriteLow(GPIOE, GPIO_PIN_0);
+            //on = !on;
+            //if(on) GPIO_WriteHigh(GPIOD, GPIO_PIN_3); else GPIO_WriteLow(GPIOD, GPIO_PIN_3);
             time = milis();
             //printf("%ld\n", time);
         }
@@ -43,3 +56,4 @@ int main(void)
 
 /*-------------------------------  Assert -----------------------------------*/
 #include "__assert__.h"
+
